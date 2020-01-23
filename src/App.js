ac146f57586c6157234a8432a3aecb75b18c9669
s19010 <s19010@std.it-college.ac.jp>
2020-01-23 14:22:09 +0900

    オートコンプリートのコンボボックス

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { TextField, Card } from '@material-ui/core'
 import { Autocomplete } from '@material-ui/lab'
 
 const App = props => {
-  const data = ['a', 'b', 'c']
+  const data = [
+    { name: 'tamashiro', age: 19 },
+    { name: 'namihira', age: 19 },
+    { name: 'kuniyoshi', age: 19 }
+  ]
   return (
     <Card>
       <Autocomplete
         id='combo-box-demo'
         options={data}
-        // getOptionLabel={option => option.title}
+        getOptionLabel={option => option.name}
         style={{ width: 300 }}
         renderInput={params => (
           <TextField
