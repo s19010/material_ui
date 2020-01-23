20857852833165181414d9060fc307ec75d6988f
s19010 <s19010@std.it-college.ac.jp>
2020-01-23 15:25:14 +0900

    カードAPI

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import React from 'react'
-import { TextField, Card } from '@material-ui/core'
+import { TextField, Card, CardActions, CardContent } from '@material-ui/core'
 import { Autocomplete } from '@material-ui/lab'
 
-const App = props => {
-  const data = [
-    { name: 'tamashiro', age: 19 },
-    { name: 'namihira', age: 19 },
-    { name: 'kuniyoshi', age: 19 }
-  ]
-  return (
-    <Card>
-      <Autocomplete
-        id='combo-box-demo'
-        options={data}
-        getOptionLabel={option => option.name}
-        style={{ width: 300 }}
-        renderInput={params => (
-          <TextField
-            {...params}
-            label='Combo box'
-            variant='outlined'
-            fullWidth
-          />
-        )}
-      />
-    </Card>
-  )
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      items: [],
+      item: ''
+    }
+  }
+
+  render () {
+    return (
+      <Card>
+        <CardActions></CardActions>
+        <CardContent></CardContent>
+      </Card>
+    )
+  }
 }
 
 export default App
